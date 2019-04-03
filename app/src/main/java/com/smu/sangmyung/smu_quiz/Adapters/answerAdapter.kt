de25ff5cbe59b3ae89eace4b40f98cea222a8a3b
pTina <gaeun2937@gmail.com>
2019-04-03 16:40:53 +0900

    데일리퀴즈-1_미완성

---
+++ app/src/main/java/com/smu/sangmyung/smu_quiz/Adapters/answerAdapter.kt
@@ -0,0 +1,9 @@
+package com.smu.sangmyung.smu_quiz.Adapters
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.smu.sangmyung.smu_quiz.MainActivity
+
+public class answerAdapter
