86284b614d0f95f7672eb91816ff684c5d1b7558
jinsu0294 <jinsususu@gmail.com>
2019-05-19 07:37:50 +0900

    google login

--- app/src/main/java/com/smu/sangmyung/smu_quiz/MainActivity.kt
+++ app/src/main/java/com/smu/sangmyung/smu_quiz/MainActivity.kt
@@ -3,11 +3,16 @@ package com.smu.sangmyung.smu_quiz
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -15,5 +20,9 @@ class MainActivity : AppCompatActivity() {
             val intent=Intent(applicationContext,GoogleSignInActivity::class.java)
             startActivity(intent)
         }
+
     }
+
 }
+
+
